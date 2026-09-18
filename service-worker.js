3602de9f9008916873c3ee05851db4987a4c196c
bosmulski <bosmulski@yahoo.com>
2026-09-18 10:00:13 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener("install", event => {
 
             return cache.addAll([
                 "./",
-                "./ConfessionWebApp4.html",
+                "./index.html",
                 "./manifest.json"
             ]);
 
